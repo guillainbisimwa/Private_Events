b7e22d268c7182941fcbb3afd050068d5634ca47
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-11-17 14:02:06 -0400

    Add test for show view, for 200 status code. Update create method on users controller to instantiate a new user object.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
     
     def new
+        @user = User.new
     end
 
     def create
+			@user = User.new(user_params)
+			if @user.save
+					redirect_to users_path
+			else
+					render :new
+			end
     end
 
     def show
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,9 +8,21 @@ RSpec.describe UsersController do
         expect(response).to render_template("new")
       end
 
+      it "has a 200 status code" do
+        get :new
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "GET show" do
       it "renders the show template" do
         get :show
         expect(response).to render_template("show")
+      end
+
+      it "has a 200 status code" do
+        get :show
+        expect(response.status).to eq(200)
       end
     end
 end
