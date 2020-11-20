82449c43d929d12dd24fe51484baa116a7cbb8a3
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-11-20 19:22:39 -0400

    Update the Events Controller to sanitize some routes to only allow existing events to be access to its show page and if it's invalid then be rerouted to the index page for events"

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,31 +1,41 @@
 class EventsController < ApplicationController
+
+  before_action :require_user, except: [:index, :show]
   
-    def index
-        @events = Event.all
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.creator = current_user.id
+
+    if @event.save
+      flash[:notice] = "#{@event.title} event has been created!"
+      redirect_to events_path
+    else
+      flash[:alert] = @event.errors.full_messages
+      redirect_to "new"
     end
+  end
 
-    def new
-        @event = Event.new
+  def show
+    if Event.find_by(id: params[:id].to_i).nil?
+      redirect_to events_path
+    else
+      @event = Event.find(params[:id].to_i)
+      @user = User.find(@event.creator)
     end
+  end
 
-    def create
-        @event = Event.new(event_params)
-		if @event.save
-			flash[:notice] = "#{@event.title} event has been created!"
-			redirect_to events_path
-		else
-			flash[:alert] = @event.errors.full_messages
-			redirect_to "new"
-		end
-    end
+  private
 
-    def show
-        @event = Event.find(params[:id].to_i)
-    end
+  def event_params
+    params.require(:event).permit(:description)
+  end
 
-    private
-
-    def event_params
-      params.require(:event).permit(:title, :location, :datetime, :description)
-	end  
 end
