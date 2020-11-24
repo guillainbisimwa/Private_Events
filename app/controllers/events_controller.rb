0ac75c18839490dd8df46f9bf83e6c13a0ea8ce5
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-24 22:52:49 +0200

    Separate Upcomming and attended events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -35,7 +35,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:description)
+    params.require(:event).permit(:description, :datetime)
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ApplicationRecord
     belongs_to :creator_id, foreign_key: :creator, class_name: 'User'
     has_many :attendees, foreign_key: :event_id, class_name: 'EventUser'
     has_many :users, through: :event_users
+
+    def self.past
+        where(self.'datetime' < DateTime.now)
+
+    end
+
+    def self.future
+
+    end
 end
