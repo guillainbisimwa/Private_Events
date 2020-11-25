f58b9f07398aeaecd7282caad9b88f732b1c5c19
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-11-25 17:12:24 -0400

    Update sessions controller create method to display a flash message in case the first name params is not a register user. Update the user model to have the attended events plurarize. Update the users show views to have it call the attended events

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,17 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(id: (params[:session][:number_field]).to_i)
 
-    @user = @user.nil? ? User.find_by(first_name: (params[:session][:first_name]).downcase) : @user
+    if @user.nil? 
+      @user = User.find_by(first_name: (params[:session][:first_name]).downcase)
+      if @user.nil?
+        redirect_to login_path, alert: 'There was something wrong with your login details' and return
+      end 
+    end
+
     if @user
       session[:user_id] = @user.id
       flash[:notice] = 'Logged in successfully'
       redirect_to users_path(@user)
-
-    else
-      flash.now[:alert] = 'There was something wrong with your login details'
-      render 'new'
     end
   end
 
--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -27,17 +27,18 @@ RSpec.describe 'Welcome to Private Events app', type: :system do
 
   describe "the go the process", type: :system do
  
-    # before :example do
-    #   User.create(first_name: 'joseph')
-    # end
-    subject { 
+    before(:example) do
       User.create(first_name: 'joseph')
-    }
+    end
+    # subject { 
+    #   User.create(first_name: 'joseph')
+    # }
 
     it "sign me in" do
       visit '/'
       click_link 'Click here to log in'
       sleep(5)
+      
       expect(page).to have_content 'Welcome to the Login Page of Private Events app'
 
       element = find(:css, "input[id$='session_first_name']")
@@ -45,9 +46,8 @@ RSpec.describe 'Welcome to Private Events app', type: :system do
 
       click_button 'Login user'
       sleep(5)
-      subject
 
-      expect(page).to have_content 'Logged in successfully'
+      expect(page).to have_content 'Hello joseph to show page'
 
     end
   end
