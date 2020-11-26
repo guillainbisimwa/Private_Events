20e0f15c60474c1096caa2369de54f1e3d90bfe8
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-26 22:19:37 +0200

    Delete unused file

--- lib/20201119220645_add_event_user_relations.rb
+++
@@ -1,6 +0,0 @@
-class AddEventUserRelations < ActiveRecord::Migration[6.0]
-  def change
-    add_foreign_key :event_users, :events, column: :event_id
-    add_foreign_key :event_users, :users, column: :user_id
-  end
-end
